2303460b2651c0f4bb18862e72484667ee8c2c8f
Carlos L. Gonzalez - Prodanet <carlos.gonzalez@prodanet.com>
2026-04-23 19:52:51 +0100

    Refactor AzureAdapterFactory to use AzureBlobStorageAdapter and remove SeekableAzureBlobStorageAdapter

--- src/AzureAdapterFactory.php
+++ src/AzureAdapterFactory.php
@@ -3,6 +3,7 @@
 namespace Prodanet\ShopwareFlysystemAzureBundle;
 
 use AzureOss\Storage\Blob\BlobServiceClient;
+use AzureOss\Storage\BlobFlysystem\AzureBlobStorageAdapter;
 use Shopware\Core\Framework\Adapter\Filesystem\Adapter\AdapterFactoryInterface;
 
 class AzureAdapterFactory implements AdapterFactoryInterface
@@ -17,7 +18,10 @@ class AzureAdapterFactory implements AdapterFactoryInterface
         $storageAccountName = $config['storage_account_name'];
         $storageAccountKey = $config['storage_account_key'];
         $storageAccountContainer = $config['storage_account_container'];
+        $storagePreffix = $config['storage_prefix'] ?? '';
         $connectionString = "DefaultEndpointsProtocol=https;AccountName=$storageAccountName;AccountKey=$storageAccountKey;EndpointSuffix=core.windows.net";
-        return new SeekableAzureBlobStorageAdapter(BlobServiceClient::fromConnectionString($connectionString), $storageAccountContainer);
+        $service = BlobServiceClient::fromConnectionString($connectionString);
+        $container = $service->getContainerClient($storageAccountContainer);
+        return new AzureBlobStorageAdapter($container, $storagePreffix);
     }
 }
